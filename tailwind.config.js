0fbee88efaab272378c358239cb25fb71078c9d4
Lázaro <lazaroyaesta@gmail.com>
2024-10-06 14:47:06 +0200

    feat: Add Tailwindcss & under construction page

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        "bungee-shade": "#FFBE28",
+      },
+    },
+  },
+  plugins: [],
+};
