f9b5202b2abdd6eff0b479f0d0bfdaf311fef495
Lázaro <lazaroyaesta@gmail.com>
2024-10-29 23:50:20 +0100

    feat: add know-more section still in progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeroComponent } from './components/hero/hero.component';
+import { KnowMoreComponent } from './components/know-more/know-more.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgOptimizedImage, NavbarComponent, HeroComponent],
+  imports: [
+    RouterOutlet,
+    NgOptimizedImage,
+    NavbarComponent,
+    HeroComponent,
+    KnowMoreComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/components/know-more/know-more.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-know-more',
+  standalone: true,
+  imports: [],
+  templateUrl: './know-more.component.html',
+  styleUrl: './know-more.component.css'
+})
+export class KnowMoreComponent {
+
+}
