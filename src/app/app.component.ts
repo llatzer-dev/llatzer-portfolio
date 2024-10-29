37bed1d4b1211f8d8a88b149a7ceb711e7757cf6
Lázaro <lazaroyaesta@gmail.com>
2024-10-29 20:19:56 +0100

    feat: add hero section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeroComponent } from './components/hero/hero.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgOptimizedImage, NavbarComponent],
+  imports: [RouterOutlet, NgOptimizedImage, NavbarComponent, HeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
