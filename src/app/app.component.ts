aa983f35fe2da53da36c52c628491028749555dc
Lázaro <lazaroyaesta@gmail.com>
2024-10-30 22:16:40 +0100

    feat: add paths

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { HeroComponent } from './components/hero/hero.component';
-import { KnowMoreComponent } from './components/know-more/know-more.component';
+import { NavbarComponent } from '@app/components/navbar/navbar.component';
+import { HeroComponent } from '@app/components/hero/hero.component';
+import { KnowMoreComponent } from '@app/components/know-more/know-more.component';
 
 @Component({
   selector: 'app-root',
