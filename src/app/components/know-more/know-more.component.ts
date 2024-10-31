9cb8a5a3bbc84eb3d46059cd677f6434ef8b8cc7
Lázaro <lazaroyaesta@gmail.com>
2024-10-31 20:30:11 +0100

    feat: add infinite card scroll

--- src/app/components/know-more/know-more.component.ts
+++ src/app/components/know-more/know-more.component.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-know-more',
   standalone: true,
   imports: [],
   templateUrl: './know-more.component.html',
-  styleUrl: './know-more.component.css'
+  styleUrls: ['./know-more.component.css'],
 })
-export class KnowMoreComponent {
+export class KnowMoreComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    const scrollers = document.querySelectorAll('.scroller');
 
+    // If a user hasn't opted in for reduced motion, then we add the animation
+    if (!window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
+      this.addAnimation(scrollers);
+      this.addHoverListeners(scrollers); // Call the function to add listeners
+    }
+  }
+
+  addAnimation(scrollers: NodeListOf<Element>) {
+    scrollers.forEach((scroller) => {
+      // Add data-animated="true" to every `.scroller`
+      scroller.setAttribute('data-animated', 'true');
+
+      // Make an array from the elements within `.scroller-inner`
+      const scrollerInner = scroller.querySelector('.scroller__inner');
+      const scrollerContent = Array.from(scrollerInner?.children || []);
+
+      // For each item in the array, clone it and append it
+      scrollerContent.forEach((item) => {
+        const duplicatedItem = item.cloneNode(true) as HTMLElement;
+        duplicatedItem.setAttribute('aria-hidden', 'true');
+        scrollerInner?.appendChild(duplicatedItem);
+      });
+    });
+  }
+
+  // Updated function to handle hover
+  addHoverListeners(scrollers: NodeListOf<Element>) {
+    scrollers.forEach((scroller) => {
+      const listItems = scroller.querySelectorAll('.tag-list li');
+      const scrollerInner = scroller.querySelector(
+        '.scroller__inner'
+      ) as HTMLElement;
+
+      listItems.forEach((item) => {
+        item.addEventListener('mouseenter', () => {
+          // Change the animation state to paused
+          scrollerInner.style.animationPlayState = 'paused';
+        });
+
+        item.addEventListener('mouseleave', () => {
+          // Reset the animation state
+          scrollerInner.style.animationPlayState = 'running';
+        });
+      });
+    });
+  }
 }
