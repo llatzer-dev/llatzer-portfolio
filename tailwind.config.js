be5656745205ef56dac1db6b91158199c0bce402
Lázaro <lazaroyaesta@gmail.com>
2024-11-05 20:02:05 +0100

    feat: add change styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,16 @@ module.exports = {
     extend: {
       colors: {
         "bungee-shade": "#FFBE28",
+        "primary-black": "#000",
+        "secondary-black": "#222",
+        "tertiary-black": "#323232",
+        "primary-white": "#fff",
+        "secondary-white": "#a1a1aa",
       },
       height: { 68: "17rem" },
+      backgroundImage: {
+        "text-gradient": "linear-gradient(90deg, #323232, #FFBE28, #323232)",
+      },
     },
   },
   plugins: [],
